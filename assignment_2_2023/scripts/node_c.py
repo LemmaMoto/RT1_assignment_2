32539ca185f93a8ebcc5f7868d3a87493f16da64
LemmaMoto <emanuelebuaodetti@gmail.com>
2024-01-06 13:36:46 +0100

    class node_c

--- assignment_2_2023/scripts/node_c.py
+++ assignment_2_2023/scripts/node_c.py
@@ -5,51 +5,46 @@ import math
 from assignment_2_2023.msg import Vel
 from assignment_2_2023.srv import Ave_pos_vel, Ave_pos_velResponse
 
-def get_distance_and_averagevelocity(msg):
-    
-    global success, average_vel_x, distance
+class InfoService:
+    def __init__(self):
+        self.average_vel_x = 0
+        self.distance = 0
 
-    des_x = rospy.get_param('/des_pos_x')
-    des_y = rospy.get_param('/des_pos_y')
+        rospy.init_node('info_service')
+        rospy.loginfo("Info service node initialized")
 
-    velocity_window_size = rospy.get_param('/window_size')
-    
-    actual_x = msg.pos_x
-    actual_y = msg.pos_y
-    
-    des_coordinates = [des_x, des_y]
-    actual_coordinates = [actual_x, actual_y]
-    distance = math.dist(des_coordinates, actual_coordinates)
+        rospy.Service("info_service", Ave_pos_vel, self.get_values)
+        rospy.Subscriber("/pos_vel", Vel, self.get_distance_and_averagevelocity)
 
-    if isinstance(msg.vel_x, list):
-        vel_data = msg.vel_x[-velocity_window_size:]
-    else:
-        vel_data = [msg.vel_x]
+    def get_distance_and_averagevelocity(self, msg):
+        des_x = rospy.get_param('/des_pos_x')
+        des_y = rospy.get_param('/des_pos_y')
 
-    average_vel_x = sum(vel_data) / min(len(vel_data), velocity_window_size)
-
-    print("Distance= ", distance, "Average velocity = ", average_vel_x)
-
+        velocity_window_size = rospy.get_param('/window_size')
         
-def get_values(_):      
-    global average_vel_x, distance
-    
-    print("Distance= ", distance, "Average velocity = ", average_vel_x)
+        actual_x = msg.pos_x
+        actual_y = msg.pos_y
+        
+        des_coordinates = [des_x, des_y]
+        actual_coordinates = [actual_x, actual_y]
+        self.distance = math.dist(des_coordinates, actual_coordinates)
 
-    
-    return Ave_pos_velResponse(distance, average_vel_x)		      
+        if isinstance(msg.vel_x, list):
+            vel_data = msg.vel_x[-velocity_window_size:]
+        else:
+            vel_data = [msg.vel_x]
 
-def info_service_main():
-    global velocity_window_size
+        self.average_vel_x = sum(vel_data) / min(len(vel_data), velocity_window_size)
 
-    rospy.init_node('info_service')
-    rospy.loginfo("Info service node initialized")
-    
-    rospy.Service("info_service", Ave_pos_vel, get_values)
+        print("Distance= ", self.distance, "Average velocity = ", self.average_vel_x)
 
-    rospy.Subscriber("/pos_vel", Vel, get_distance_and_averagevelocity)
+    def get_values(self, _):      
+        print("Distance= ", self.distance, "Average velocity = ", self.average_vel_x)
+        return Ave_pos_velResponse(self.distance, self.average_vel_x)		      
 
-    rospy.spin()
+    def spin(self):
+        rospy.spin()
 
 if __name__ == "__main__":
-    info_service_main()
+    service = InfoService()
+    service.spin()
